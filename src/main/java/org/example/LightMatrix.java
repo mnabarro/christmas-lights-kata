f6b242cebb58fdb4b3afff1c274782c9ffb2845f
Matias Nabarro <matiasnabarro@gmail.com>
2022-09-05 15:43:27 +0200

    refactor: extracted repeated code from LightMatrix class

--- src/main/java/org/example/LightMatrix.java
+++ src/main/java/org/example/LightMatrix.java
@@ -6,30 +6,33 @@ public class LightMatrix {
     protected final ArrayList<ArrayList<Light>> matrix;
     protected final Integer width;
     protected final Integer height;
+    public interface SetLightStatus {
+        void changer(Integer column, Integer row);
+    }
+    void setStatusRange(Integer colFrom, Integer rowFrom, Integer colTo, Integer rowTo, SetLightStatus status) {
+        for (int i = colFrom; i < colTo + 1; i++) {
+            for (int j = rowFrom; j < rowTo + 1; j++) {
+                status.changer(i, j);
+            }
+        }
+    }
 
     public LightMatrix(Integer width, Integer height) {
         this.matrix = new ArrayList<>();
         this.width = width;
         this.height = height;
     }
-
     public Integer getValue(Integer column, Integer row) {
 
         return matrix.get(row).get(column).getValue();
     }
-
     public void turnOn(Integer column, Integer row) {
 
         matrix.get(row).get(column).turnOn();
 
     }
-
     public void turnOnRange(Integer columnFrom, Integer rowFrom, Integer columnTo, Integer rowTo) {
-        for (int i = columnFrom; i < columnTo + 1; i++) {
-            for (int j = rowFrom; j < rowTo + 1; j++) {
-                turnOn(i, j);
-            }
-        }
+        setStatusRange(columnFrom, rowFrom, columnTo, rowTo, this::turnOn);
     }
 
     public void turnOff(Integer column, Integer row) {
@@ -39,11 +42,7 @@ public class LightMatrix {
     }
 
     public void turnOffRange(Integer columnFrom, Integer rowFrom, Integer columnTo, Integer rowTo) {
-        for (int i = columnFrom; i < columnTo + 1; i++) {
-            for (int j = rowFrom; j < rowTo + 1; j++) {
-                turnOff(i, j);
-            }
-        }
+        setStatusRange(columnFrom, rowFrom, columnTo, rowTo, this::turnOff);
     }
 
     public void toggle(Integer column, Integer row) {
@@ -53,10 +52,6 @@ public class LightMatrix {
     }
 
     public void toggleRange(Integer columnFrom, Integer rowFrom, Integer columnTo, Integer rowTo) {
-        for (int i = columnFrom; i < columnTo + 1; i++) {
-            for (int j = rowFrom; j < rowTo + 1; j++) {
-                toggle(i, j);
-            }
-        }
+        setStatusRange(columnFrom, rowFrom, columnTo, rowTo, this::toggle);
     }
 }
